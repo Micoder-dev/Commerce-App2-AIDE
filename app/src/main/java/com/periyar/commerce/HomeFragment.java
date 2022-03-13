768c2b0cc634cbabe56e43eb2cd2c65ff9fe56f4
Micoder-dev <vetrimaran1439@gmail.com>
2022-03-12 20:26:04 -0800

    all sem cardview 4 activities created

--- app/src/main/java/com/periyar/commerce/HomeFragment.java
+++ app/src/main/java/com/periyar/commerce/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.periyar.commerce;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
@@ -21,10 +23,49 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private CardView cardSem1;
+    private CardView cardSem2;
+    private CardView cardSem3;
+    private CardView cardSem4;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_home, container, false);
+
+        cardSem1=(CardView) view.findViewById(R.id.cardSem1);
+        cardSem1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),Sem1.class);
+                startActivity(intent);
+            }
+        });
+        cardSem2=(CardView) view.findViewById(R.id.cardSem2);
+        cardSem2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),Sem2.class);
+                startActivity(intent);
+            }
+        });
+        cardSem3=(CardView) view.findViewById(R.id.cardSem3);
+        cardSem3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),Sem3.class);
+                startActivity(intent);
+            }
+        });
+        cardSem4=(CardView) view.findViewById(R.id.cardSem4);
+        cardSem4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),Sem4.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 
