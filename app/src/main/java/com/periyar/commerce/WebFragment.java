cf73201371c16c1b512f79064e44fb609c29e01c
Micoder-dev <vetrimaran1439@gmail.com>
2022-03-16 08:23:21 -0700

    webview download enabled while clicking the download btn it will opens the chrome browser and starts download

--- app/src/main/java/com/periyar/commerce/WebFragment.java
+++ app/src/main/java/com/periyar/commerce/WebFragment.java
@@ -1,13 +1,17 @@
 package com.periyar.commerce;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.DownloadListener;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -33,10 +37,19 @@ public class WebFragment extends Fragment {
         webSettings.setJavaScriptEnabled(true);
         //Force links & redirects to open in the WebView instead of in a browser
         mWebView.setWebViewClient(new WebViewClient());
-
         mWebView.getSettings().setAppCacheEnabled(true);
-
         mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.setWebChromeClient(new WebChromeClient());
+
+        mWebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String s, String s1, String s2, String s3, long l) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(s));
+                startActivity(i);
+            }
+        });
+
         //webview can go back
         mWebView.canGoBack();
         mWebView.setOnKeyListener(new View.OnKeyListener() {
